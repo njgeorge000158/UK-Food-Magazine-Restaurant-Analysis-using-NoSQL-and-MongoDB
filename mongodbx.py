ddad46bbb2de69f6694ea8f3972dc5af2671a2f4
Nicholas George <137228821+njgeorge000158@users.noreply.github.com>
2024-05-02 12:55:29 -0700

    Add files via upload

--- mongodbx.py
+++ mongodbx.py
@@ -13,8 +13,10 @@
  #      for completing common tasks with MongoDB databases. 
  #      Here is the list:
  #
+ #      insert_dictionary_list_into_collection
  #      insert_json_file_into_collection
  #      delete_records_then_insert_dictionary
+ #      return_table_as_dataframe
  #
  #
  #  Date            Description                             Programmer
@@ -25,6 +27,10 @@
 
 import json
 
+import pandas as pd
+
+pd.options.mode.chained_assignment = None
+
 
 # In[2]:
 
@@ -33,6 +39,60 @@ CONSTANT_LOCAL_FILE_NAME = 'mongodbx.py'
 
 
 # In[3]:
+
+
+#*******************************************************************************************
+ #
+ #  Function Name:  insert_dictionary_list_into_collection
+ #
+ #  Function Description:
+ #      This function inserts a dictionary or list of dictionaries into a compatible
+ #      MongoDB collection.
+ #
+ #
+ #  Return Type: n/a
+ #
+ #
+ #  Function Parameters:
+ #
+ #  Type    Name            Description
+ #  -----   -------------   ----------------------------------------------
+ #  pymongo pymongo_collection
+ #                          The parameter is the pymongo collection.
+ #  list    collection_dictionary_list
+ #                          The parameter is the dictionary or dictionary list.
+ #
+ #
+ #  Date                Description                                 Programmer
+ #  ---------------     ------------------------------------        ------------------
+ #  09/18/2023          Initial Development                         Nicholas J. George
+ #
+ #******************************************************************************************/
+
+def insert_dictionary_list_into_collection \
+        (pymongo_collection,
+         collection_dictionary_list):
+    
+    if isinstance(collection_dictionary_list, list):
+
+        list_length_integer = len(collection_dictionary_list)
+
+        if list_length_integer > 0 and isinstance(collection_dictionary_list[0], dict):
+
+            if list_length_integer > 1:
+            
+                pymongo_collection.insert_many(collection_dictionary_list)
+
+            elif list_length_integer == 1:
+
+                pymongo_collection.insert_one(collection_dictionary_list[0])
+    
+    elif isinstance(collection_dictionary_list, dict):
+    
+        pymongo_collection.insert_one(input_dictionary_list)
+
+
+# In[4]:
 
 
 #*******************************************************************************************
@@ -70,18 +130,12 @@ def insert_json_file_into_collection \
     with open(file_path_string) as json_file:
     
         collection_dictionary_list = json.load(json_file)
+
             
-        
-    if isinstance(collection_dictionary_list, list):
-            
-        pymongo_collection.insert_many(collection_dictionary_list)
-            
-    else:
-            
-        pymongo_collection.insert_one(collection_dictionary_list)
+    insert_dictionary_list_into_collection(pymongo_collection, collection_dictionary_list)
 
 
-# In[4]:
+# In[5]:
 
 
 #*******************************************************************************************
@@ -135,6 +189,48 @@ def delete_records_then_insert_dictionary \
     
     
     pymongo_collection.insert_one(new_dictionary)
+
+
+# In[6]:
+
+
+#*******************************************************************************************
+ #
+ #  Function Name:  return_table_as_dataframe
+ #
+ #  Function Description:
+ #      This function queries a table for its contents and returns the results 
+ #      as a dataframe.
+ #
+ #
+ #  Return Type: n/a
+ #
+ #
+ #  Function Parameters:
+ #
+ #  Type    Name            Description
+ #  -----   -------------   ----------------------------------------------
+ #  pymongo pymongo_collection
+ #                          The parameter is the pymongo collection.
+ #
+ #
+ #  Date                Description                                 Programmer
+ #  ---------------     ------------------------------------        ------------------
+ #  09/18/2023          Initial Development                         Nicholas J. George
+ #
+ #******************************************************************************************/
+
+def return_table_as_dataframe(pymongo_collection):
+
+    query_results_dictionary_list = pymongo_collection.find()
+
+    
+    temp_dataframe = pd.DataFrame(query_results_dictionary_list)
+    
+    temp_dataframe.drop(columns = '_id', axis = 1, inplace = True)
+
+    
+    return temp_dataframe
 
 
 # In[ ]:
